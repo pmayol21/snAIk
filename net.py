35b1ea1fc9bb19e95a99096b3c58592ab2fbd7e2
richardnorthrup <46721717+richardnorthrup@users.noreply.github.com>
2019-08-06 15:19:32 -0400

    Add files via upload

---
+++ net.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.e ** -x)
+
+class NueralNet:
+    def __init__(self, topology):
+        self.topology = topology
+
+        self.numInputNodes = topology[0]
+        self.numOutputNodes = topology[-1]
+        self.numHiddenLayers = len(topology)-2
+        self.totalLayers = len(topology)
+
+        self.weights = list() #each element is a matrix of weights
+
+    def randomizeWeights(self):
+        #if there are n layers, there are n-1 sets of weights
+        for i in range (0, self.totalLayers-1):
+            self.weights.append( np.random.rand(self.topology[i], self.topology[i]) ) #rows in num of nodes in next layer, columns in num rows in previous4
+
+        print("W0:", self.weights[0])
+
+    #does not implement bias atm. Param is column vector of input activations
+    def feedForward(self, inputActivations):
+        activations = list()
+        activations.append( inputActivations )
+        for i in range(1, self.totalLayers): #start at first hidden layer
+            activations[i] = sigmoid(np.dot(activations[i-1], self.weights[i-1]))
+
+############# TESTING #########
+
+top = [4, 3, 2] #layer 0 (input) has 4 nodes, layers 1 (a hidden) has 3 nodes
+network = NueralNet(top)
+network.randomizeWeights()
+
+input = np.array([1, 0, 1, 1])
+input = input.transpose()
+
+network.feedForward(input)
