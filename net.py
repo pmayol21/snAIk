9391db6497b24feeaa117b35da8a5f3b26042f93
Josh Lebedinsky <joshlebed@gmail.com>
2019-08-06 15:31:03 -0400

    fixed multiplication order

--- net.py
+++ net.py
@@ -9,18 +9,17 @@ class NueralNet:
         # topology[i] is an integer corresponding to the number of nodes
         # in layer i of the network
         self.topology = topology
-
         self.numInputNodes = topology[0]
         self.numOutputNodes = topology[-1]
         self.numHiddenLayers = len(topology)-2
         self.totalLayers = len(topology)
-
-        self.weights = list() #each element is a matrix of weights
+        #each element is a matrix of weights
+        self.weights = []
 
     def randomizeWeights(self):
-        #if there are n layers, there are n-1 sets of weights
-        for i in range (0, self.totalLayers-1):
-            self.weights.append( np.random.rand(self.topology[i+1], self.topology[i]) ) #rows in num of nodes in next layer, columns in num rows in previous4
+        # if there are n layers, there are n-1 sets of weights
+        for i in range(0, self.totalLayers - 1):
+            self.weights.append(np.random.rand(self.topology[i + 1], self.topology[i]) ) #rows in num of nodes in next layer, columns in num rows in previous4
 
         print("W0:", self.weights[0])
 
@@ -29,7 +28,7 @@ class NueralNet:
         activations = list()
         activations.append( inputActivations )
         for i in range(1, self.totalLayers): #start at first hidden layer
-            activations.append (sigmoid(np.dot(activations[i-1], self.weights[i-1])) )
+            activations.append (sigmoid(np.dot(self.weights[i - 1], activations[i - 1])) )
 
 ############# TESTING #########
 
