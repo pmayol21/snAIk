a3e44c705fad87640868daff67abb9c7980774ad
pmayol21 <pmayol21@gmail.com>
2019-08-07 00:27:17 -0400

    added testing file and cross over

---
+++ tests.py
@@ -0,0 +1,31 @@
+import genetic_methods
+import net
+import numpy as np
+
+
+# --------------------------TESTING FEED FORWARD-------------------------------
+print("TESTING FEED FORWARD")
+top = [4, 3, 2] #layer 0 (input) has 4 nodes, layers 1 (a hidden) has 3 nodes
+network = net.NeuralNet(top)
+network.randomizeWeights()
+
+input = np.array([1, 0, 1, 1])
+input = input.transpose()
+print(network.feedForward(input))
+# -----------------------------------------------------------------------------
+
+
+# --------------------------TESTING CROSSOVER-------------------------------
+print("TESTING CROSSOVER")
+
+top = [4, 3, 2] #layer 0 (input) has 4 nodes, layers 1 (a hidden) has 3 nodes
+network1 = net.NeuralNet(top)
+network1.randomizeWeights()
+network2 = net.NeuralNet(top)
+network2.randomizeWeights()
+
+crossed_works = genetic_methods.CrossOver(network1, network2)
+print(len(crossed_works))
+print(crossed_works)
+# -----------------------------------------------------------------------------
+
